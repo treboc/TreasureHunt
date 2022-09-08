5f02f5af05fa7e74d4ea0c73b49654d4089cc076
Dominik Hauser <dom@dasdom.de>
2022-09-08 18:45:19 +0200

    Initial Commit

---
+++ TreasureHunt/TreasureHuntApp.swift
@@ -0,0 +1,14 @@
+//  Created by Dominik Hauser on 08.09.22.
+//  
+//
+
+import SwiftUI
+
+@main
+struct TreasureHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
