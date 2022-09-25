fa915dc0dae481835d4caa383c106fdea908e8cb
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-09-25 17:54:05 +0200

    [Add] Button initializer, where Label is an SFSymbol

---
+++ TreasureHunt/Extensions/Button+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Button+Extension.swift
+//  TriomiCount
+//
+//  Created by Marvin Lee Kobert on 12.05.22.
+//
+
+import SwiftUI
+
+/// https://www.swiftbysundell.com/tips/swiftui-extensions-using-generics/
+extension Button where Label == Image {
+  init(iconName: String, action: @escaping () -> Void) {
+    self.init(action: action) {
+      Image(systemName: iconName)
+    }
+  }
+}
