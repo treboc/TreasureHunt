fc4bc19a9b3323a197c02779d3162d55f63989dd
Marvin Lee <48476808+treboc@users.noreply.github.com>
2022-09-26 13:40:07 +0200

    [Add] SettingsView

---
+++ TreasureHunt/Views/SettingsView/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  TreasureHunt
+//
+//  Created by Marvin Lee Kobert on 26.09.22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+  var body: some View {
+    Text("Settings")
+  }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+  static var previews: some View {
+    SettingsView()
+  }
+}
