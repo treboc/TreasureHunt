b338c296c29bda502f52c34eaf48b2d311a339e4
Marvin Lee Kobert <marvin.kobert@me.com>
2022-10-02 13:07:22 +0200

    [UI] updated View

--- TreasureHunt/Views/HuntView/TrafficWarningView.swift
+++ TreasureHunt/Views/HuntView/TrafficWarningView.swift
@@ -9,39 +9,33 @@ import SwiftUI
 
 struct TrafficWarningView: View {
   @Binding var warningRead: Bool
-
+  
   var body: some View {
-    ZStack {
-      Color.black
-        .opacity(0.5)
-        .ignoresSafeArea()
-
-      VStack(spacing: 30) {
-        HStack {
-          Text("Achtung")
-            .font(.system(.largeTitle, design: .rounded))
-            .fontWeight(.black)
-          Spacer()
-          Text("⚠️")
-            .font(.largeTitle)
-        }
-
-        Text("Achte bei der Suche immer auf deine Umgebung und den Verkehr.")
-          .frame(maxWidth: .infinity, alignment: .leading)
-
-        Button("Verstanden") {
-          withAnimation {
-            warningRead = true
-          }
-        }
-        .buttonStyle(.borderedProminent)
-        .padding(.bottom)
+    VStack(spacing: 30) {
+      HStack {
+        Text("Achtung")
+          .font(.system(.largeTitle, design: .rounded))
+          .fontWeight(.black)
+        Spacer()
+        Text("⚠️")
+          .font(.largeTitle)
       }
-      .padding()
-      .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
-      .padding()
+      
+      Text("Achte bei der Suche immer auf deine Umgebung und den Verkehr.")
+        .frame(maxWidth: .infinity, alignment: .leading)
+      
+      Button("Verstanden") {
+        withAnimation {
+          warningRead = true
+        }
       }
+      .buttonStyle(.borderedProminent)
+      .padding(.bottom)
     }
+    .padding()
+    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
+    .padding()
+  }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
