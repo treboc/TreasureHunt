b08590945b7d3fcdac797f06046a739fffe7cd18
Marvin Lee Kobert <marvin.kobert@me.com>
2022-10-29 09:58:53 +0200

    [Add] ReadSize modifier, which allow to read the size of any SwiftUI view

--- TreasureHunt/Extensions/ViewModifier/ReadSize+ViewModifier.swift
+++ TreasureHunt/Extensions/ViewModifier/ReadSize+ViewModifier.swift
@@ -5,4 +5,22 @@
 //  Created by Marvin Lee Kobert on 28.10.22.
 //
 
-import Foundation
+import SwiftUI
+
+struct SizePreferenceKey: PreferenceKey {
+  static var defaultValue: CGSize = .zero
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
+
+extension View {
+  func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+    background(
+      GeometryReader { geo in
+        Color.clear
+          .preference(key: SizePreferenceKey.self, value: geo.size)
+      }
+    )
+    .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+  }
+}
+
